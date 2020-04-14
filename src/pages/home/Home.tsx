f75e0f5fa1afe5a492050f48eb19c129276a7f14
Jerry Ejonavi <jerryasih@gmail.com>
2020-04-14 19:01:56 +0100

    feat(): prefer functional component

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { FC } from 'react';
 import styles from './Home.module.scss';
 import { Row, Col, Typography, Button, Divider } from 'antd';
 import { RouteComponentProps } from 'react-router';
@@ -41,98 +41,95 @@ interface HomeProps extends RouteComponentProps {
     dispatch(args: AnyAction): void;
 }
 
-class Home extends Component<Partial<HomeProps>> {
+const Home: FC<Partial<HomeProps>> = () => {
 
-    render(): JSX.Element {
-        return (
-            <>
+    return (
+        <>
+            <Helmet>
+                <title>
+                    Whiteboard - {constants.DEFAULT_TITLE}
+                </title>
+            </Helmet>
 
-                <Helmet>
-                    <title>
-                        Whiteboard - {constants.DEFAULT_TITLE}
-                    </title>
-                </Helmet>
+            <div className={styles.home}>
 
-                <div className={styles.home}>
+                {/* WELCOME */}
 
-                    {/* WELCOME */}
-
-                    <div className={styles.welcome}>
-                        <Row align="middle" style={{ height: '100vh' }}>
-                            <Col span={24}>
-                                <Row gutter={[0,64]}>
-                                    <Col
-                                        xs={{ span: 24 }} lg={{ span: 12 }}
-                                        style={{
-                                            display: 'flex',
-                                            alignItems: 'center',
-                                            justifyContent: 'center',
-                                        }}
-                                    >
-                                        <div className={styles.text}>
-                                            <Title level={1}>
-                                                Whiteboard
-                                            </Title>
-                                            <Text>
-                                                Create, collaborate in real-time and share your ideas.
-                                            </Text>
-                                        </div>
-                                    </Col>
-                                    <Col xs={{ span: 24 }} lg={{ span: 12 }}>
-                                        <div className={styles.image + ' shadow-md p-4 text-center'}>
-                                            <img src="/assets/character_drawing.svg" alt="character drawing" />
-                                        </div>
-                                    </Col>
-                                    <Col span={24}>
-                                        <div className="text-center">
-                                            <Link to="/login">
-                                                <Button type="primary" shape="round" className="hover:shadow-md">
-                                                    Get started
-                                                </Button>
-                                            </Link>
-                                        </div>
-                                    </Col>
-                                </Row>
-                            </Col>
-                        </Row>
-                    </div>
-
-                    {/* FEATURES */}
-
-                    <div className={styles.features}>
-                        <div className="text-center">
-                            <Divider>
-                                <Title level={1}>
-                                    Features
-                                </Title>
-                            </Divider>
-                        </div>
-
-                        <Row gutter={[12,24]} justify="center">
-                            {appFeatures.map((feature, idx) => (
-                                <Col xs={{ span: 24 }} lg={{ span: 8 }} key={idx}>
-                                    <div className={styles.feature}>
-                                        <div className="text-center p-8">
-                                            {(feature.imageSrc) && (
-                                                <img src={feature.imageSrc} alt={feature.title} />
-                                            )}
-                                            <Title level={3} className="my-4">
-                                                {feature.title}
-                                            </Title>
-                                            <Text className="my-1">
-                                                {feature.description}
-                                            </Text>
-                                        </div>
+                <div className={styles.welcome}>
+                    <Row align="middle" style={{ height: '100vh' }}>
+                        <Col span={24}>
+                            <Row gutter={[0,64]}>
+                                <Col
+                                    xs={{ span: 24 }} lg={{ span: 12 }}
+                                    style={{
+                                        display: 'flex',
+                                        alignItems: 'center',
+                                        justifyContent: 'center',
+                                    }}
+                                >
+                                    <div className={styles.text}>
+                                        <Title level={1}>
+                                            Whiteboard
+                                        </Title>
+                                        <Text>
+                                            Create, collaborate in real-time and share your ideas.
+                                        </Text>
                                     </div>
                                 </Col>
-                            ))}
-                        </Row>
+                                <Col xs={{ span: 24 }} lg={{ span: 12 }}>
+                                    <div className={styles.image + ' shadow-md p-4 text-center'}>
+                                        <img src="/assets/character_drawing.svg" alt="character drawing" />
+                                    </div>
+                                </Col>
+                                <Col span={24}>
+                                    <div className="text-center">
+                                        <Link to="/login">
+                                            <Button type="primary" shape="round" className="hover:shadow-md">
+                                                Get started
+                                            </Button>
+                                        </Link>
+                                    </div>
+                                </Col>
+                            </Row>
+                        </Col>
+                    </Row>
+                </div>
+
+                {/* FEATURES */}
+
+                <div className={styles.features}>
+                    <div className="text-center">
+                        <Divider>
+                            <Title level={1}>
+                                Features
+                            </Title>
+                        </Divider>
                     </div>
 
+                    <Row gutter={[12,24]} justify="center">
+                        {appFeatures.map((feature, idx) => (
+                            <Col xs={{ span: 24 }} lg={{ span: 8 }} key={idx}>
+                                <div className={styles.feature}>
+                                    <div className="text-center p-8">
+                                        {(feature.imageSrc) && (
+                                            <img src={feature.imageSrc} alt={feature.title} />
+                                        )}
+                                        <Title level={3} className="my-4">
+                                            {feature.title}
+                                        </Title>
+                                        <Text className="my-1">
+                                            {feature.description}
+                                        </Text>
+                                    </div>
+                                </div>
+                            </Col>
+                        ))}
+                    </Row>
                 </div>
-            </>
-        );
-    }
-}
+
+            </div>
+        </>
+    );
+};
 
 export default connect()(Home);
