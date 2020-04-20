2392131490ad7f5b1cdd50a86520ad60dbca5622
Jerry Ejonavi <jerryasih@gmail.com>
2020-04-21 00:52:11 +0100

    chore(utils): update messages

--- src/utils/appMessages.ts
+++ src/utils/appMessages.ts
@@ -1,1 +1,2 @@
 export const FORM_VALIDATION_FAILED = 'Please check that all fields have been filled correctly.';
+export const REQUEST_FAILED_DEFAULT = 'There was a problem processing your request';
