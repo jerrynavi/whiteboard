4c4a4dae3fd43c4a66ebf2a1fc9ed3b3d8122c42
Jerry Ejonavi <jerryasih@gmail.com>
2020-04-21 01:07:25 +0100

    wip(login): add signup form, routing

--- src/pages/login/Login.tsx
+++ src/pages/login/Login.tsx
@@ -4,18 +4,57 @@ import { connect } from 'react-redux';
 import Helmet from 'react-helmet';
 import Progress from 'react-topbar-progress-indicator';
 import styles from './Login.module.scss';
+import { State } from '../../interfaces/state.interface';
+import { Link } from 'react-router-dom';
 
-const { Title, Text } = Typography;
-
+const SignupForm = lazy(() => import('../../components/signup-form/SignupForm'));
 const LoginForm = lazy(() => import('../../components/login-form/LoginForm'));
 const LoginSidebar = lazy(() => import('../../components/login-sidebar/LoginSidebar'));
 
-export const Login: FC = () => {
+const { Title, Text } = Typography;
+
+const LOGIN_URL = '/login';
+const SIGNUP_URL = '/login?action=signup';
+
+const RenderSignupLink = (): JSX.Element => (
+    <Link to={{
+        pathname: SIGNUP_URL,
+    }}>
+        <Text type="secondary">
+            Create an account
+        </Text>
+    </Link>
+);
+
+const RenderLoginLink = (): JSX.Element => (
+    <Link to={{
+        pathname: LOGIN_URL,
+    }}>
+        <Text type="secondary">
+            I already have an account
+        </Text>
+    </Link>
+);
+
+interface Props {
+    currentUrl: string;
+}
+
+export const Login: FC<Props> = (props) => {
+
+    const { currentUrl } = props;
 
     return (
         <>
             <Helmet>
-                <title>Login</title>
+                <title>
+                    {(currentUrl === LOGIN_URL)
+                        ? 'Login'
+                        : (currentUrl === SIGNUP_URL)
+                            ? 'Create account'
+                            : ''
+                    }
+                </title>
             </Helmet>
 
             <Row
@@ -43,6 +82,16 @@ export const Login: FC = () => {
                 {/* Form */}
                 <Col xs={{ span: 24, order: 0 }} md={{ span: 12, order: 1 }} style={{ height: '100%' }}>
                     <div className={styles.form}>
+                        <div className="flex flex-row p-8">
+                            <div className="ml-auto">
+                                {(currentUrl !== SIGNUP_URL) && (
+                                    <RenderSignupLink />
+                                )}
+                                {(currentUrl !== LOGIN_URL) && (
+                                    <RenderLoginLink />
+                                )}
+                            </div>
+                        </div>
                         <div className="p-4 py-16 text-center">
                             <div className="my-8">
                                 <Title level={1}>
@@ -52,9 +101,22 @@ export const Login: FC = () => {
                                     </Text>
                                 </Title>
                             </div>
-                            <Suspense fallback={<Progress />}>
-                                <LoginForm />
-                            </Suspense>
+                            <div
+                                style={{
+                                    maxWidth: '400px',
+                                    margin: '0 auto',
+                                }}
+                            >
+                                <Suspense fallback={<Progress />}>
+                                    {(currentUrl === LOGIN_URL) && (
+                                        <LoginForm />
+                                    )}
+
+                                    {(currentUrl === SIGNUP_URL) && (
+                                        <SignupForm />
+                                    )}
+                                </Suspense>
+                            </div>
                         </div>
                     </div>
                 </Col>
@@ -64,4 +126,10 @@ export const Login: FC = () => {
     );
 };
 
-export default connect()(Login);
+const mappedProps = (state: State): { currentUrl: string } => {
+    return {
+        currentUrl: state.app.currentUrl,
+    };
+};
+
+export default connect(mappedProps)(Login);
