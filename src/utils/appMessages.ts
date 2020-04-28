b5dafd6655e74bcb67ae08b7e03cae000f83eb6f
Jerry Ejonavi <jerryasih@gmail.com>
2020-04-28 15:03:11 +0100

    chore(app): update msgs, fix css issue with ant icon btn

--- src/utils/appMessages.ts
+++ src/utils/appMessages.ts
@@ -1,2 +1,4 @@
 export const FORM_VALIDATION_FAILED = 'Please check that all fields have been filled correctly.';
 export const REQUEST_FAILED_DEFAULT = 'There was a problem processing your request';
+export const CANVAS_NOT_EMPTY = 'Please clear canvas and try again.';
+export const SAVE_SUCCESS = 'Saved successfully!';
